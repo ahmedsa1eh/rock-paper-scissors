2a759d0ae4be0b9b06c5f6c7e0abf1a2cf6af48a
Ahmed Saleh <ahmed1a1a3a100@gmail.com>
2024-02-19 14:01:03 +0200

    Add user and pc scores

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ let pcImg = document.querySelector(".pc-img");
 let imgUser = Array.from(document.querySelectorAll(".img-user"));
 let result = document.querySelector(".result");
 let preventPlaying = document.querySelector(".disabled");
+// let scoreEl = document.querySelector(".score")
+let userScoreEl = document.querySelector(".user-score");
+let pcScoreEl = document.querySelector(".pc-score");
+
+let scoreUser = 0;
+let scorePc = 0;
+userScoreEl.textContent = scoreUser;
+pcScoreEl.textContent = scorePc;
 // generate pc choice randomly
 let randomNum = Math.trunc(Math.random() * 3);
 pcImg.src = imgUser[randomNum].src;
@@ -35,18 +43,27 @@ imgUser.forEach((img) => {
     if (pcChoice === userChoice) {
       console.log("تعادل");
       result.textContent = "تعادل";
+      scoreUser++;
+      scorePc++;
+      userScoreEl.textContent = scoreUser;
+      pcScoreEl.textContent = scorePc;
       preventPlaying.classList.add("show");
       setTimeout(reset, 3000);
     } else if (userChoice == "rock") {
       if (pcChoice == "scissors") {
         console.log("user won");
         result.textContent = "فوز";
+        scoreUser++;
+        userScoreEl.textContent = scoreUser;
         preventPlaying.classList.add("show");
 
         setTimeout(reset, 3000);
       } else if (pcChoice == "paper") {
         console.log("user lost");
         result.textContent = "خسارة";
+
+        scorePc++;
+        pcScoreEl.textContent = scorePc;
         preventPlaying.classList.add("show");
 
         setTimeout(reset, 3000);
@@ -55,6 +72,8 @@ imgUser.forEach((img) => {
       if (pcChoice == "paper") {
         console.log("user win");
         result.textContent = "فوز";
+        scoreUser++;
+        userScoreEl.textContent = scoreUser;
         preventPlaying.classList.add("show");
 
         setTimeout(reset, 3000);
@@ -62,21 +81,24 @@ imgUser.forEach((img) => {
         console.log("user lost");
         result.textContent = "خسارة";
         preventPlaying.classList.add("show");
-
+        scorePc++;
+        pcScoreEl.textContent = scorePc;
         setTimeout(reset, 3000);
       }
     } else if (userChoice == "paper") {
       if (pcChoice == "rock") {
         console.log("user win");
         preventPlaying.classList.add("show");
-
+        scoreUser++;
+        userScoreEl.textContent = scoreUser;
         result.textContent = "فوز";
         setTimeout(reset, 3000);
       } else if (pcChoice == "scissors") {
         console.log("user lost");
         result.textContent = "خسارة";
         preventPlaying.classList.add("show");
-
+        scorePc++;
+        pcScoreEl.textContent = scorePc;
         setTimeout(reset, 3000);
       }
     }
